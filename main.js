e2d8df9afcec31b742292606fe1f0b8bf9de8d26
Shafique-brohi <161844016+Shafique-brohi@users.noreply.github.com>
2024-03-30 16:44:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    { message: "select your first number", type: "number", name: "firstNumber" },
+    { message: "select your second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "multiplication", "subtraction", "division"],
+    }
+]);
+if (answers.operator === "addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+;
